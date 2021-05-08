58900f23f4d1d5b523a275f2a6c2285fab5b1e63
sabiinsearch <sabiinsearch@gmail.com>
2021-05-09 00:52:52 +0530

    update

---
+++ BinaryTreeDemo.java
@@ -0,0 +1,34 @@
+import java.util.*; 
+import java.io.*;
+
+class Main {
+
+  public static String TreeConstructor(String[] strArr) {
+    // code goes here  
+    String msg = true;
+    String[] argument = new String [strArr.size()];
+    argument = strArr;
+    List<int[]> families = new ArrayList();
+    Scanner s = new Scanner(argument[0]);
+    
+
+
+
+
+
+    return msg;
+  }
+
+
+
+
+
+
+
+  public static void main (String[] args) {  
+    // keep this function call here     
+    Scanner s = new Scanner(System.in);
+    System.out.print(TreeConstructor(s.nextLine())); 
+  }
+
+}
